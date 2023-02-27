77777c3212651c8cd3965039433224ebbe7b7c91
PYDAMNAIDUDDOGGA <107361429+pydamnaidudogga@users.noreply.github.com>
2023-02-27 13:28:59 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 // const uri = 'mongodb://localhost/placement_cell';
-const uri = "mongodb+srv://pydamnaidu:T3qby2N9N0lOaMyq@cluster0.22bxq8o.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://pydamnaidu:D1OBXUD9rcUXnqwn@cluster0.oltart9.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set('strictQuery',false);
 mongoose.connect(uri,{useNewUrlParser: true});
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console,"Error connecting to the mongoodb"));
 db.once('open',function(){
     console.log('success in conecting to the mongoDB');
-
 });
 
 module.exports = db;
