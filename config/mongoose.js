29e8e22290f1be0e3ba6a0e620919d853e430030
PYDAMNAIDUDDOGGA <107361429+pydamnaidudogga@users.noreply.github.com>
2023-02-28 14:52:49 +0530

    Update mongoose.js
    changing mongoose url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const uri = 'mongodb://localhost/placement_cell';
-const uri = "mongodb+srv://pydamnaidu:D1OBXUD9rcUXnqwn@cluster0.oltart9.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://pydamnaidu:D1OBXUD9rcUXnqwn@database.cjh6ckr.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set('strictQuery',false);
 mongoose.connect(uri,{useNewUrlParser: true});
 const db = mongoose.connection;
